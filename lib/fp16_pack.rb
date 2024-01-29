cac3a31c0b8c022bce722967a0b36f574931c060
Maciek Dubiński <maciek@dubinski.net>
2024-01-29 21:11:51 +0100

    Expand comments

--- lib/fp16_pack.rb
+++ lib/fp16_pack.rb
@@ -3,5 +3,6 @@
 require_relative "fp16_pack/version"
 require_relative "fp16_pack/fp16_pack"
 
+# Fp16Pack module, defined so it can house methods defined in the native extension.
 module Fp16Pack
 end
